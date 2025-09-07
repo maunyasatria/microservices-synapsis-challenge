fb55db2f548b0fffaf46bef174f55b5f8ac47da4
maunyasatria <maunyasatria@gmail.com>
2025-09-07 13:51:58 +0700

    sql file doesnt needed

--- inventory.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE order_items (
-  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-  order_id uuid REFERENCES orders(id) ON DELETE CASCADE,
-  sku text NOT NULL,
-  qty int NOT NULL,
-  unit_price bigint,
-  created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP
-);
